45bd9fb5dcade39e82d28db146808b2764648ad1
Jorge Gutierrez <jorgericardogutierrez4@gmail.com>
2025-06-09 21:27:52 +0200

    Mejorando el archivo setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="calculadora-matematica",
+    name="simulador-cartas",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[],
-    author="Tu Nombre",
-    author_email="tu.email@ejemplo.com",
-    description="Una librería de calculadoras matemáticas",
+    author="Jorge Gutierrez",
+    author_email="jorgericardogutierrez4@gmail.com",
+    description="Una librería para simular una baraja de cartas",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/tu-usuario/calculadora-matematica",
+    url="https://github.com/JorgeGutierrez3/simulador-cartas",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
